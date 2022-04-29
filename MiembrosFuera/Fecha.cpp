0cb095bb6668efc8e2a65e846867013780935fe5
Jesus Conde <educaleft@gmail.com>
2022-04-29 09:43:22 +0100

    Agregar archivos de proyecto.

---
+++ MiembrosFuera/Fecha.cpp
@@ -0,0 +1,35 @@
+#include "Fecha.h"
+
+// Constructor Fecha
+Fecha::Fecha(int dia, int mes, int anho)
+{
+    setFecha(dia, mes, anho);
+}
+
+// Función miembro Fecha 
+void Fecha::setFecha(int dia, int mes, int anho)
+{
+    m_mes = mes;
+    m_anho = anho;
+    m_dia = dia;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
